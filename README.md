030cbde8a1495aae4ad50015ddbac6380bcf9568
Josh Gum <revgum@gmail.com>
2017-11-22 11:09:32 -0800

    efs mounted drive for shared storage packer postgres database setup provisioning using the vpc nat (bastion)

--- README.md
+++ README.md
@@ -7,21 +7,29 @@
 - Run Terraform plan to confirm updates
 - Run Terraform apply to deploy
 
+## Packer
 ### Run Packer to create a new AMI
+Packer is used to build several different images for the infrastructure. A description of each is as follows;
+- packer/aws/**base** : Basic server build with application dependencies and user setup, all other builds should reference this as their starting AMI.
+- packer/aws/**web** : OD2 Rails web server & application
+- packer/aws/**database** : OD2 Postgresql database server
+
 ```
-$cd packer/web
+$export BUILD_TYPE=base
+$cd packer/aws/$BUILD_TYPE
 
 # Validate the build is going to work
-$packer validate -var-file ../variables.json web.json
+$packer validate -var-file ../../variables.json $BUILD_TYPE.json
 
 # Build the AMI
-$packer build -var-file ../variables.json web.json
+$packer build -var-file ../variables.json $BUILD_TYPE.json
 
-# Wait for awhile and capture the AMI after a successful build
+# Wait for awhile and capture the AMI and output details after a successful build
 ```
-
+## Terraform
+Terraform files are found in the `terraform/aws` directory.
 ### Update Terraform variables
-Open variables.tf and update the AMI located in the *web_amis* hash for the appropriate AWS region.
+Open `terraform/aws/variables.tf` and update the AMI located in either **web_amis**, or **db_amis** hash for the appropriate AWS region.
 
 ### Run Terraform plan to confirm the updates
 ```
---
+++ terraform/aws/vpc.tf
@@ -0,0 +1,227 @@
+resource "aws_vpc" "default" {
+  cidr_block = "${var.vpc_cidr}"
+  enable_dns_hostnames = true
+  tags {
+    Name = "${var.application_name} vpc"
+  }
+}
+
+resource "aws_internet_gateway" "default" {
+  vpc_id = "${aws_vpc.default.id}"
+}
+
+/*
+ * Packer security group for builds needing EFS mount automation
+ */
+resource "aws_security_group" "packer_builder" {
+  name = "${var.application_name} packer"
+  description = "Allow traffic necessary for packer builder"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 2049
+    to_port = 2049
+    protocol = "tcp"
+    cidr_blocks = ["${var.private_subnet_cidr}"]
+  }
+  egress {
+    from_port = 2049
+    to_port = 2049
+    protocol = "tcp"
+    cidr_blocks = ["${var.private_subnet_cidr}"]
+  }
+  egress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  vpc_id = "${aws_vpc.default.id}"
+
+  tags {
+    Name = "${var.application_name} packer"
+  }
+}
+
+/*
+ * NAT Instance
+ */
+resource "aws_security_group" "nat" {
+  name = "${var.application_name} vpc nat"
+  description = "Allow traffic to pass from the private subnet to the internet"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["${var.private_subnet_cidr}"]
+  }
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["${var.private_subnet_cidr}"]
+  }
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["${var.vpc_cidr}"]
+  }
+  egress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  vpc_id = "${aws_vpc.default.id}"
+
+  tags {
+    Name = "${var.application_name} vpc nat"
+  }
+}
+
+resource "aws_instance" "nat" {
+  ami = "${lookup(var.vpc_amis, var.aws_region)}" # this is a special ami preconfigured to do NAT
+  availability_zone = "us-west-2a"
+  instance_type = "t2.micro"
+  key_name = "${var.aws_key_name}"
+  vpc_security_group_ids = ["${aws_security_group.nat.id}"]
+  subnet_id = "${aws_subnet.us-west-2a-public.id}"
+  associate_public_ip_address = true
+  source_dest_check = false
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              EOF
+  tags {
+    Name = "${var.application_name} vpc nat"
+  }
+}
+
+resource "aws_eip" "nat" {
+  instance = "${aws_instance.nat.id}"
+  vpc = true
+}
+
+/*
+  Public Subnet
+*/
+resource "aws_subnet" "us-west-2a-public" {
+  vpc_id = "${aws_vpc.default.id}"
+
+  cidr_block = "${var.public_subnet_cidr}"
+  availability_zone = "us-west-2a"
+
+  tags {
+    Name = "${var.application_name} public subnet"
+  }
+}
+
+resource "aws_route_table" "us-west-2a-public" {
+  vpc_id = "${aws_vpc.default.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.default.id}"
+  }
+
+  tags {
+    Name = "${var.application_name} public subnet"
+  }
+}
+
+resource "aws_route_table_association" "us-west-2a-public" {
+  subnet_id = "${aws_subnet.us-west-2a-public.id}"
+  route_table_id = "${aws_route_table.us-west-2a-public.id}"
+}
+
+/*
+  Private Subnet
+*/
+resource "aws_subnet" "us-west-2a-private" {
+  vpc_id = "${aws_vpc.default.id}"
+
+  cidr_block = "${var.private_subnet_cidr}"
+  availability_zone = "us-west-2a"
+
+  tags {
+    Name = "${var.application_name} private subnet"
+  }
+}
+
+resource "aws_route_table" "us-west-2a-private" {
+  vpc_id = "${aws_vpc.default.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    instance_id = "${aws_instance.nat.id}"
+  }
+
+  tags {
+    Name = "${var.application_name} private subnet"
+  }
+}
+
+resource "aws_route_table_association" "us-west-2a-private" {
+  subnet_id = "${aws_subnet.us-west-2a-private.id}"
+  route_table_id = "${aws_route_table.us-west-2a-private.id}"
+}
+
+output "nat_public_ssh" {
+  value = "ssh -i ${var.aws_key_path} -A ec2-user@${aws_eip.nat.public_ip}"
+}
+
+output "nat_copy_ssh_key" {
+  value = "scp -i ${var.aws_key_path} ${var.aws_key_path} ec2-user@${aws_eip.nat.public_ip}:~/.ssh/"
+}
+
+output "us_west_2a_private_subnet_id" {
+  value = "${aws_subnet.us-west-2a-private.id}"
+}
+
+output "us_west_2a_public_subnet_id" {
+  value = "${aws_subnet.us-west-2a-public.id}"
+}
+
+output "aws_security_group_packer" {
+  value = "${aws_security_group.packer_builder.id}"
+}
